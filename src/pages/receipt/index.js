df93181ea49469458e5bf32616170a7e0489923a
YangTuanAnh <yangtuananh2003@gmail.com>
2022-07-04 16:41:15 +0700

    Receipt screen

---
+++ src/pages/receipt/index.js
@@ -0,0 +1,16 @@
+Page({
+    onLoad(query) {},
+    onReady() {},
+    onShow() {},
+    onHide() {},
+    onUnload() {},
+    returnHome(event) {
+        my.createSelectorQuery()
+            .select(event.currentTarget.id)
+            .exec((ret) => {
+                my.navigateBack({
+                    url: "pages/home/index"
+                })
+            })
+    }
+});
