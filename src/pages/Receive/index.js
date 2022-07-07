7a5925781db41f95f41ad07ff6d975df5bfb6f96
YangTuanAnh <yangtuananh2003@gmail.com>
2022-07-07 19:55:02 +0700

    Fix Send button

--- src/pages/Receive/index.js
+++ src/pages/Receive/index.js
@@ -1,91 +1,86 @@
 Page({
 
-  data: {
-      showOption1: false,
-      showOption2: false,
-      selected1: "23kg under",
-      items1: ["23kg Over", "On 23kg", "Options different"],
+    data: {
+        showOption1: false,
+        showOption2: false,
+        selected1: "23kg under",
+        items1: ["23kg Over", "On 23kg", "Options different"],
 
-      imgs: undefined,
-      
-      vehicle: 'motobike'
-  },
+        imgs: undefined,
 
-  onSelect1(selected1) {
-    this.setData({
-        selected1
-    });
-},
+        vehicle: 'motobike'
+    },
+
+    onSelect1(selected1) {
+        this.setData({
+            selected1
+        });
+    },
 
 
     onTap(event) {
+        my.navigateBack()
+    },
+
+
+    onChooseImage() {
+        my.chooseImage({
+            count: 5,
+            success: (res) => {
+                console.log(res);
+                this.setData({
+                    imgs: res.filePaths
+                });
+            },
+            fail: (e) => {
+                console.log(e);
+            }
+        });
+    },
+
+    onClickFinished1(e) {
+        this.setData({
+            showOption1: true
+        })
+        my.showTabBar({
+            animation: false
+        })
+    },
+
+    onClickFinished2(e) {
+        this.setData({
+            showOption2: true
+        })
+        my.showTabBar({
+            animation: false
+        })
+    },
+
+    onClickClose1(e) {
+        this.setData({
+            showOption1: false
+        })
+        my.showTabBar({
+            animation: true
+        })
+    },
+
+    onClickClose2(e) {
+        this.setData({
+            showOption2: false
+        })
+        my.showTabBar({
+            animation: true
+        })
+    },
+
+    pageLogin(event) {
         my.createSelectorQuery()
             .select(event.currentTarget.id)
             .exec((ret) => {
-                if (event.currentTarget.id == 'btn_send')
-                    my.navigateBack()
+                my.navigateTo({
+                    url: "pages/login/index"
+                })
             })
-          },
-          
-
-          onChooseImage() {
-            my.chooseImage({
-              count: 5,
-              success: (res) => {
-                console.log(res);
-                this.setData({
-                  imgs: res.filePaths
-                });
-              },
-              fail: (e) => {
-                console.log(e);
-              }
-            });
-          },
-
-          onClickFinished1(e) {
-            this.setData ({
-              showOption1 : true
-            })
-            my.showTabBar ({
-              animation : false
-            })
-           },
-
-           onClickFinished2(e) {
-            this.setData ({
-              showOption2 : true
-            })
-            my.showTabBar ({
-              animation : false
-            })
-           },
-           
-           onClickClose1(e){
-             this.setData ({
-               showOption1 : false
-             })
-             my.showTabBar({
-               animation: true
-             })
-           },
-
-           onClickClose2(e){
-            this.setData ({
-              showOption2 : false
-            })
-            my.showTabBar({
-              animation: true
-            })
-          },
-
-          pageLogin(event){
-            my.createSelectorQuery()
-            .select(event.currentTarget.id)
-            .exec((ret) => {
-              my.navigateTo({
-                url: "pages/login/index"
-              })
-            })
-          }
+    }
 })
