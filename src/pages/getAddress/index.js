855a9818fc76e4f88f4f2222416a9d734dd383e1
Kaicity <80609391+Kaicity@users.noreply.github.com>
2022-07-08 16:30:41 +0700

    edit07/08

--- src/pages/getAddress/index.js
+++ src/pages/getAddress/index.js
@@ -1,11 +1,24 @@
+const init = () => ({
+  latitude: 10.779693436530149,
+  longitude: 106.67971686137946,
+  markers: [],
+  polygon: [],
+  polyline: [],
+  zoom: 10,
+  circles: []
+});
+
+
 Page({
   data: {
-    location: undefined,
+    //location: undefined,
+    apiKey: 'AIzaSyDY6f-OwKoI7g7VtWUcBhKKieXfyHKBHw8',
+    ...init(),
 
     street: 'Nguyễn Văn Luông',
     city: 'TPHCM',
-    district: "6",
-    ward: "Center",
+    district: 6,
+    ward: 12,
     full_name: 'Nguyễn Minh Thông',
     phone_number: '0703338458'
     
@@ -14,15 +27,15 @@ Page({
     // districtErrorMsg: 'Vui lòng nhập quận',
     // wardErrorMsg: 'Vui lòng nhập phường',
   },
-  /*
+  
   onChangeAddress(address) {
     console.log('address :', address);
   },
   onFullAddress(address) {
     console.log('full address :', address);
-  }, */
+  }, 
 
-  onGetLocation() {
+  /*onGetLocation() {
     my.getLocation({
       cacheTimeout: 30,
       type: 1,
@@ -32,6 +45,105 @@ Page({
       fail: (e) => {
         console.log(e);
       }
+    });
+  } */
+  resetMap() {
+    this.setData({ ...init() });
+  },
+  onMarkerTap(event) {
+    console.log('onMarkerTap', event);
+  },
+  onTap(event) {
+    console.log('onTap', event);
+  },
+  showPolyline() {
+    this.setData({
+      zoom: 3,
+      latitude: 0,
+      longitude: -180,
+      polyline: [
+        {
+          strokeColor: '#FF0000',
+          strokeWidth: 2,
+          points: [
+            { latitude: 37.772, longitude: -122.214 },
+            { latitude: 21.291, longitude: -157.821 },
+            { latitude: -18.142, longitude: 178.431 },
+            { latitude: -27.467, longitude: 153.027 }
+          ]
+        }
+      ]
+    });
+  },
+  showPolygon() {
+    this.setData({
+      zoom: 5,
+      latitude: 24.886,
+      longitude: -70.268,
+      polygon: [
+        {
+          strokeColor: '#FF0000',
+          strokeWidth: 2,
+          fillColor: '#FF0000',
+          points: [
+            { latitude: 25.774, longitude: -80.19 },
+            { latitude: 18.466, longitude: -66.118 },
+            { latitude: 32.321, longitude: -64.757 },
+            { latitude: 25.774, longitude: -80.19 }
+          ]
+        }
+      ]
+    });
+  },
+  showCircle() {
+    const citymap = {
+      chicago: {
+        latitude: 41.878,
+        longitude: -87.629,
+        population: 2714856
+      },
+      newyork: {
+        latitude: 40.714,
+        longitude: -74.005,
+        population: 8405837
+      },
+      losangeles: {
+        latitude: 34.052,
+        longitude: -118.243,
+        population: 3857799
+      },
+      vancouver: {
+        latitude: 49.25,
+        longitude: -123.1,
+        population: 603502
+      }
+    };
+    this.setData({
+      zoom: 4,
+      latitude: 41.878,
+      longitude: -87.629,
+      circles: Object.keys(citymap).map((key) => ({
+        strokeColor: '#FF0000',
+        strokeWidth: 2,
+        fillColor: '#FF0000',
+        latitude: citymap[key].latitude,
+        longitude: citymap[key].longitude,
+        radius: Math.sqrt(citymap[key].population) * 100
+      }))
+    });
+  },
+  showMarker() {
+    this.setData({
+      zoom: 4,
+      latitude: -25.363,
+      longitude: 131.044,
+      markers: [
+        {
+          id: '1',
+          latitude: -25.363,
+          longitude: 131.044
+        }
+      ]
     });
   }
 });
